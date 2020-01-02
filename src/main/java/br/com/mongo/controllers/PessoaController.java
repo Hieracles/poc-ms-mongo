d18f4b1f2acbaa1d0ccceddd10d2bbe3411a163f
hieracles <hieracles@hotmail.com>
2020-01-02 15:10:12 -0200

    Commit inicial.

---
+++ src/main/java/br/com/mongo/controllers/PessoaController.java
@@ -0,0 +1,56 @@
+package br.com.mongo.controllers;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import br.com.mongo.domain.PessoaDomain;
+import br.com.mongo.service.PessoaService;
+
+@RestController
+@RequestMapping(path = "mongo/pessoas", produces = "application/json")
+public class PessoaController {
+	
+	@Autowired
+	private PessoaService pessoaService;
+	
+	@GetMapping()
+	public ResponseEntity<List<PessoaDomain>> listar() throws TimeoutException, JsonParseException, JsonMappingException, IOException{
+		return ResponseEntity.ok().body(pessoaService.listar());		
+	}
+	
+	@PostMapping
+	public ResponseEntity<PessoaDomain> inserir(@RequestBody PessoaDomain p){
+		
+		PessoaDomain retorno = pessoaService.inserir(p);		
+		return ResponseEntity.ok().body(retorno);
+		
+	}
+	
+	@PutMapping(path="/{codigo}") 
+	public ResponseEntity<PessoaDomain> alterar(@PathVariable("codigo") String codigo, @RequestBody PessoaDomain p){
+		
+		p.set_id(codigo);
+		PessoaDomain retorno = pessoaService.alterar(p);		
+		
+		return ResponseEntity.ok().body(retorno);
+		
+	}
+
+}
---
+++ src/main/java/br/com/mongo/service/PessoaService.java
@@ -0,0 +1,34 @@
+package br.com.mongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mongo.domain.PessoaDomain;
+import br.com.mongo.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public List<PessoaDomain> listar(){
+		
+		return pessoaRepository.findAll(); 
+		
+	}
+	
+	public PessoaDomain inserir(PessoaDomain p){
+		pessoaRepository.insert(p);			
+		return p;
+	}
+	
+	public PessoaDomain alterar(PessoaDomain p){
+		pessoaRepository.save(p);			
+		return p;
+	}	
+	
+}
